46858e4dc8de172de38094fd41d6c7b6ecb5b210
André van Heerwaarde <avheerwaarde@periplus.nl>
2021-01-25 17:37:32 +0100

    Use correct integer type when calculating values

--- SqlParser/SelectData/SelectDataFromBinaryScalarExpression.cs
+++ SqlParser/SelectData/SelectDataFromBinaryScalarExpression.cs
@@ -28,6 +28,21 @@ namespace SqlMemoryDb.SelectData
                 ReturnType = typeof( string );
                 _DbType = DbType.String;
             }
+            else if ( typeLeft == typeof(byte) && typeRight == typeof( byte ) )
+            {
+                ReturnType = typeof( byte );
+                _DbType = DbType.Byte;
+            }
+            else if ( typeLeft == typeof( Int16 ) && typeRight == typeof( Int16 ) )
+            {
+                ReturnType = typeof( short );
+                _DbType = DbType.Int16;
+            }
+            else if ( typeLeft == typeof( Int32 ) && typeRight == typeof( Int32 ) )
+            {
+                ReturnType = typeof( int );
+                _DbType = DbType.Int32;
+            }
             else if ( HelperReflection.IsInteger(typeLeft) && HelperReflection.IsInteger(typeRight) )
             {
                 ReturnType = typeof( long );
@@ -40,6 +55,18 @@ namespace SqlMemoryDb.SelectData
             if ( _DbType == DbType.String )
             {
                 return SelectString( rows );
+            }
+            if ( _DbType == DbType.Byte )
+            {
+                return SelectByte( rows );
+            }
+            if ( _DbType == DbType.Int16 )
+            {
+                return SelectInteger16( rows );
+            }
+            if ( _DbType == DbType.Int32 )
+            {
+                return SelectInteger32( rows );
             }
             if ( _DbType == DbType.Int64 )
             {
@@ -66,10 +93,40 @@ namespace SqlMemoryDb.SelectData
             return left + right;
         }
 
-        private long SelectInteger( List<RawTableRow> rows )
+        private byte SelectByte( List<RawTableRow> rows )
         {
-            var left = Convert.ToInt64(Helper.GetValue( _Expression.Left, typeof( long ), _RawData, rows ));
-            var right = Convert.ToInt64(Helper.GetValue( _Expression.Right, typeof( long ), _RawData, rows ));
+            var left = Convert.ToByte(Helper.GetValue( _Expression.Left, typeof( byte ), _RawData, rows ));
+            var right = Convert.ToByte(Helper.GetValue( _Expression.Right, typeof( byte ), _RawData, rows ));
+            switch ( _Expression.Operator )
+            {
+                case SqlBinaryScalarOperatorType.Add: return (byte)(left + right);
+                case SqlBinaryScalarOperatorType.Subtract: return (byte)( left - right );
+                case SqlBinaryScalarOperatorType.Multiply: return (byte)( left - right );
+                case SqlBinaryScalarOperatorType.Divide: return (byte)( left / right );
+                default:
+                    throw new NotImplementedException( $"Operator {_Expression.Operator.GetType(  )} is not implemented.");
+            }
+        }
+
+        private short SelectInteger16( List<RawTableRow> rows )
+        {
+            var left = Convert.ToInt16( Helper.GetValue( _Expression.Left, typeof( short ), _RawData, rows ) );
+            var right = Convert.ToInt16( Helper.GetValue( _Expression.Right, typeof( short ), _RawData, rows ) );
+            switch ( _Expression.Operator )
+            {
+                case SqlBinaryScalarOperatorType.Add: return (short) (left + right);
+                case SqlBinaryScalarOperatorType.Subtract: return (short) (left - right);
+                case SqlBinaryScalarOperatorType.Multiply: return (short) (left * right);
+                case SqlBinaryScalarOperatorType.Divide: return (short) (left / right);
+                default:
+                    throw new NotImplementedException( $"Operator {_Expression.Operator.GetType()} is not implemented." );
+            }
+        }
+
+        private int SelectInteger32( List<RawTableRow> rows )
+        {
+            var left = Convert.ToInt32( Helper.GetValue( _Expression.Left, typeof( int ), _RawData, rows ) );
+            var right = Convert.ToInt32( Helper.GetValue( _Expression.Right, typeof( int ), _RawData, rows ) );
             switch ( _Expression.Operator )
             {
                 case SqlBinaryScalarOperatorType.Add: return left + right;
@@ -77,7 +134,23 @@ namespace SqlMemoryDb.SelectData
                 case SqlBinaryScalarOperatorType.Multiply: return left * right;
                 case SqlBinaryScalarOperatorType.Divide: return left / right;
                 default:
-                    throw new NotImplementedException( $"Operator {_Expression.Operator.GetType(  )} is not implemented.");
+                    throw new NotImplementedException( $"Operator {_Expression.Operator.GetType()} is not implemented." );
+            }
+        }
+
+
+        private long SelectInteger( List<RawTableRow> rows )
+        {
+            var left = Convert.ToInt64( Helper.GetValue( _Expression.Left, typeof( long ), _RawData, rows ) );
+            var right = Convert.ToInt64( Helper.GetValue( _Expression.Right, typeof( long ), _RawData, rows ) );
+            switch ( _Expression.Operator )
+            {
+                case SqlBinaryScalarOperatorType.Add: return left + right;
+                case SqlBinaryScalarOperatorType.Subtract: return left - right;
+                case SqlBinaryScalarOperatorType.Multiply: return left * right;
+                case SqlBinaryScalarOperatorType.Divide: return left / right;
+                default:
+                    throw new NotImplementedException( $"Operator {_Expression.Operator.GetType()} is not implemented." );
             }
         }
 
