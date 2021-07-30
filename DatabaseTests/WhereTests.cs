c0d30b576ba7e72bf8d5ab24aca757051ef23ea0
André van Heerwaarde <avheerwaarde@periplus.nl>
2021-07-30 12:10:29 +0200

    implement where like

--- DatabaseTests/WhereTests.cs
+++ DatabaseTests/WhereTests.cs
@@ -43,6 +43,35 @@ namespace DatabaseTests
             }
         }
 
+        [DataTestMethod]
+        [DataRow( null, 3 )]
+        [DataRow( "Name1", 1 )]
+        [DataRow( "Name2", 1 )]
+        [DataRow( "Not found", 0 )]
+        public void WhereClause_OptionalTestLike_ShouldFilter( string name, int count )
+        {
+            var applications = new ApplicationDto[]
+            {
+                new ApplicationDto { Name = "Name1", User = "User1", DefName = "Def1"},
+                new ApplicationDto { Name = "Name2", User = "User2", DefName = "Def2"},
+                new ApplicationDto { Name = "Name3", User = "User3", DefName = "Def3"},
+            };
+            const string sqlInsert = "INSERT INTO application ([Name], [User], [DefName]) VALUES (@Name, @User, @DefName)";
+
+            using var connection = new MemoryDbConnection();
+            connection.GetMemoryDatabase().Clear();
+            connection.Execute( SqlStatements.SqlCreateTableApplication );
+            connection.Execute( sqlInsert, applications );
+
+            string sqlSelect = $"{SqlStatements.SqlSelectApplication} WHERE @name is NULL OR [Name] LIKE @name";
+            var appsQueried = connection.Query<ApplicationDto>( sqlSelect, new { name = name } ).ToList();
+            appsQueried.Count.Should().Be( count );
+            if ( count == 1 )
+            {
+                appsQueried.First().Name.Should().Be( name );
+            }
+        }
+
         [TestMethod]
         public void WhereClause_IntParameterWithCalculation_ShouldFilter( )
         {
